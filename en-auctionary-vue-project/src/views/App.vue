896cf13e5ca192aa3b82764b2bda4c2bd527a15e
Amanda Koka <amandakoka@Amandas-MacBook-Air.local>
2025-12-02 18:45:54 +0000

    Create Vue Project and create basic page/component files

---
+++ en-auctionary-vue-project/src/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+import Login from './pages/Login.vue';
+import Home from './pages/Home.vue';
+
+export default {
+  components: {
+    Login,
+    Home
+  }
+}
+</script>
