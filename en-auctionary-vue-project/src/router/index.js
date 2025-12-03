a0c1dd5cf1a2bb388c8eb9babee383a77a5b6a97
Amanda Koka <amandakoka@Amandas-MacBook-Air.local>
2025-12-03 22:03:47 +0000

    Update user authentication and add check to pages that need authentication

--- en-auctionary-vue-project/src/router/index.js
+++ en-auctionary-vue-project/src/router/index.js
@@ -6,14 +6,26 @@ import CreateAccount from "../views/pages/CreateAccount.vue"
 import Items from "../views/pages/Items.vue"
 import Item from "../views/pages/Item.vue"
 import AddItem from "../views/pages/AddItem.vue"
+import Account from "../views/pages/Account.vue"
 
+// checks if user is authenticated and logged in, if not redirects to login page
+const ifAuthenticated = (to, from, next) => {
+    const loggedIn = localStorage.getItem("session_token");
+    if (loggedIn) {
+        next();
+        return;
+    }
+    next("/login");
+}
 const routes = [
     { path: "/", component: Home },
     { path: "/login", component: Login },
     { path: "/create_account", component: CreateAccount },
     { path: "/items", component: Items},
     { path: "/item/:id", component: Item},
-    { path: "/add_item", component: AddItem},
+    // pages only accessible if logged in
+    { path: "/add_item", component: AddItem, beforeEnter: ifAuthenticated},
+    { path: "/account", component: Account, beforeEnter: ifAuthenticated},
 ]
 
 const router = createRouter({
