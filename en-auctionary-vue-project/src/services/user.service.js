4e269cb3f44cefab1985c865d04bb8407eade1f9
Amanda Koka <amandakoka@Amandas-MacBook-Air.local>
2025-12-03 22:08:14 +0000

    Add create_account,login and logout API request functions for user endpoints.

---
+++ en-auctionary-vue-project/src/services/user.service.js
@@ -0,0 +1,97 @@
+const create_account = (first_name, last_name, email, password) => {
+    return fetch("http://localhost:3333/users",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                "password": password,
+            })
+        }
+    )
+    .then((response) => {
+        if(response.status === 201) {
+            return response.json();
+        } else if (response.status === 400) {
+            throw 'Bad Request';
+        } else {
+            throw 'Something went wrong';
+        }
+    })
+    .then((resJson) => {
+        return resJson;
+    })
+    .catch((err) => {
+        console.log("Err",err);
+        return Promise.reject(err);
+    }) 
+}
+
+const login = (email, password) => {
+    return fetch("http://localhost:3333/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password,
+            })
+        }
+    )
+    .then((response) => {
+        if(response.status === 200) {
+            return response.json();
+        } else if (response.status === 400) {
+            throw 'Bad Request';
+        } else {
+            throw 'Something went wrong';
+        }
+    })
+    .then((resJson) => {
+        localStorage.setItem("user_id", resJson.user_id);   
+        localStorage.setItem("session_token", resJson.session_token); 
+        return resJson;
+    })
+    .catch((err) => {
+        console.log("Err",err);
+        return Promise.reject(err);
+    })
+}
+
+const logout = () => {
+    return fetch("http://localhost:3333/logout",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Authorization": localStorage.getItem("session_token"),  
+            } 
+        })
+    .then((response) => {
+        if(response.status === 200) {
+            localStorage.removeItem("user_id");   
+            localStorage.removeItem("session_token"); 
+            return;
+        } else if(response.status === 401) {
+            throw "Not logged in"
+        } else {
+            throw "Something went wrong"
+        }
+    })
+    .catch((error) => {
+        console.log("Err", error);
+        return Promise.reject(error);
+    })
+}
+
+export const userService = {
+    create_account,
+    login,
+    logout,
+};
